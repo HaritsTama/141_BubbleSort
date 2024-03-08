c41d0998c2cc7aed5a9df353ee389a1296e5ec3f
HaritsTama <nayotama1818@gmail.com>
2024-03-08 11:00:51 +0700

    Menambahkan fungsi input

--- BubbleSort_141/BubbleSort_141.cpp
+++ BubbleSort_141/BubbleSort_141.cpp
@@ -3,3 +3,19 @@ using namespace std;
 
 int a[20];
 int n;
+
+void input() {
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array = ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
